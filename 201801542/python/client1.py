62c943248d4aec4e681e948b9fc9d84a52af741e
Vinicio Vieira dos Santos <68493091+VVSRevolution@users.noreply.github.com>
2022-07-02 14:44:42 -0300

    201801542

--- 201801542/python/client1.py
+++ 201801542/python/client1.py
@@ -4,7 +4,7 @@ import socket
 FORMAT = 'utf-8'
 HEADER = 64
 PORT = 8080
-EXIT = "!exit"
+EXIT = "exit"
 
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
@@ -14,14 +14,21 @@ client.connect(ADDR)
 
 
 def send(msg):
+    #print(f"[SENDING]: {msg}")
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
     client.send(message)
+    #print(client.recv(2048).decode(FORMAT))
+
+
+print(client.recv(2048).decode(FORMAT))
+run = True
+while(run):
+    msg = input()
+    send (msg)
     print(client.recv(2048).decode(FORMAT))
-
-
-send("ola")
-send("!exit")
+    if msg == EXIT:
+        run = False
